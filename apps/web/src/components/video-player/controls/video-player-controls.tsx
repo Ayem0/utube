a3acb32b37b141ac63281d91b691884bc472045a
Ayem0 <baptiste.genin1@gmail.com>
2026-05-23 19:41:24 +0200

    fix turborepo docker container + turbo repo version upgrade to 2.9.14

--- apps/web/src/components/video-player/controls/video-player-controls.tsx
+++ apps/web/src/components/video-player/controls/video-player-controls.tsx
@@ -5,7 +5,7 @@ export function VideoPlayerControls({
 }) {
   return (
     <div className="absolute bottom-0 left-0 opacity-0 right-0 transition-opacity duration-200 ease-in-out pointer-events-none group-data-active:pointer-events-auto group-data-active:opacity-100">
-      <div className="flex w-full flex-col gap-2 px-3 pb-2">{children}</div>
+      <div className="flex w-full flex-col gap-1 px-3 pb-2">{children}</div>
     </div>
   );
 }
